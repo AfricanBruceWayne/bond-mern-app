3ffaea6990ac1da3df4d962bed8714447e0729b8
Thasi Mhlongo <bruce.mhlongo21@gmail.com>
2019-11-12 11:02:45 +0200

    Added User model

---
+++ models/User.js
@@ -0,0 +1,31 @@
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, unique: true },
+  password: String,
+  profile: {
+    name: String
+  }
+}, { timestamps: true });
+
+/**
+ * Password hash middleware.
+ */
+userSchema.pre('save', function save(next) {
+  const user = this;
+  if (!user.isModified('password')) { return next(); }
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) { return next(err); }
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) { return next(err); }
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
